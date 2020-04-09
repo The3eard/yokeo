7959377f0c2336a28a4af9d4983614dff238476f
Adolfo Fuentes <pantocreitor@gmail.com>
2020-04-09 19:48:49 +0200

    primera conexion a base de datos

--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,40 @@
 /* uso de cookies para usuario */
 /* regex usuario /[a-zA-Z][a-zA-Z0-9-_]{5,20}/gi */
 /* regex mail /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi*/
+
+document
+  .querySelector("#formRegAccept")
+  .addEventListener("click", compruebaUsuario);
+
+function compruebaUsuario() {
+  let user = document.querySelector("#formRegUser").value.trim();
+  let mail = document.querySelector("#formRegMail").value.trim();
+  let regexUser = /[a-zA-Z][a-zA-Z0-9-_]{5,20}/gi;
+  let regexMail = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+  if (regexUser.test(user)) {
+    if (regexMail.test(mail)) {
+      console.log("hasta aqui1");
+      altaUserDb(user, mail);
+    } else {
+      alert("Introduzca una dirección de correo electrónico válida");
+    }
+  } else {
+    alert("Introduzca un nombre de usuario válido");
+  }
+}
+function altaUserDb(user, mail) {
+  console.log("hasta aqui2");
+  param = "user=" + user + "&mail=" + mail;
+  $.post("../php/registro.php", param, respuestaAltaUser, "json");
+}
+
+function respuestaAltaUser(json) {
+  console.log(json);
+  if (json.error) {
+    alert(json.mensaje);
+  } else {
+    alert(json.mensaje);
+    /*     frmInsertClienteFrm.reset();
+    $("#frmInsertCliente").hide("normal"); */
+  }
+}
---
+++ php/registro.php
@@ -0,0 +1,24 @@
+<?php
+$servidor = "localhost";
+$basedatos = "yoquedo";
+$usuario = "root";
+$password = "";
+$nick = $_POST['user'];
+$mail = $_POST['mail'];
+$db = new mysqli($servidor, $usuario, $password, $basedatos);
+if ($db->connect_error) {
+	die("La conexión ha fallado, error número " .
+		$db->connect_errno . ": " . $db->connect_error);
+}
+mysqli_set_charset($db, "utf8");
+$stmt = $db->prepare("INSERT INTO usuario(email, nick) VALUES (?, ?)");
+$stmt->bind_param('ss', $mail, $nick);
+$resultado = $stmt->execute();
+if ($resultado) {
+	$respuesta["error"] = 0;
+	$respuesta["mensaje"] = "Usuario registrado";
+} else {
+	$respuesta["error"] = 1;
+	$respuesta["mensaje"] = "Error " . mysqli_error($db);
+}
+echo json_encode($respuesta);
