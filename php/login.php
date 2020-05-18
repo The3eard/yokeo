6254f854deca89144658ab4fa071ca636e822b6d
Adolfo Fuentes <adolfo_fuentes@outlook.com>
2020-05-18 21:08:20 +0200

    using prettier for formatting code

--- php/login.php
+++ php/login.php
@@ -4,8 +4,8 @@ $basedatos = "yoquedo";
 $usuario = "root";
 $password = "";
 $mail = $_GET['mail'];
-$db = mysqli_connect($servidor, $usuario, $password, $basedatos)
-     or die(mysqli_error($conexion));
+($db = mysqli_connect($servidor, $usuario, $password, $basedatos)) or
+     die(mysqli_error($conexion));
 mysqli_set_charset($db, "utf8");
 $sql = "SELECT email FROM usuario WHERE email='" . $mail . "'";
 $resultado = mysqli_query($db, $sql);
