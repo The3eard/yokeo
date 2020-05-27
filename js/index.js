6c6e52b1cc80a1e61d038d87743a25f2fd21907a
Adolfo Fuentes <adolfo_fuentes@outlook.com>
2020-05-27 20:55:29 +0200

    clean code

--- js/index.js
+++ js/index.js
@@ -2,36 +2,6 @@ function toIndex() {
 	location = "../index.html";
 }
 
-/* function setCookie(cvalue, exdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-	var expires = "expires=" + d.toGMTString();
-	document.cookie = "user=" + cvalue;
-	console.log((document.cookie = "user=" + cvalue));
-}
-
-function getCookie(cname) {
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(";");
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == " ") {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
-}
-
-function delCookie() {
-	setCookie(null, 1);
-} */
-
-/* ********************* */
-
 function setCookie(cname, cvalue, exdays) {
 	var d = new Date();
 	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
